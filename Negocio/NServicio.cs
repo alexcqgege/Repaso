6f286de82e6dd3ff57421e8bbefba2a36da002d1
alexcqgege <135793705+alexcqgege@users.noreply.github.com>
2024-07-03 21:54:57 -0500

    Add files via upload

---
+++ Negocio/NServicio.cs
@@ -0,0 +1,41 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+
+namespace Negocio
+{
+    public class NServicio
+    {
+        private DServicio dServicio = new DServicio();
+
+        public String Registrar(Servicio servicio)
+        {
+            servicio.Atendido = false;
+            servicio.FechaCreacion = DateTime.Now;
+            return dServicio.Registrar(servicio);
+        }
+
+        public String Modificar(Servicio servicio)
+        {
+            return dServicio.Modificar(servicio);
+        }
+
+        public String Atender(Servicio servicio)
+        {
+            servicio.Atendido = true;
+            return dServicio.Atender(servicio);
+        }
+
+        // Eliminado Lógico
+        public String Eliminar(int servicioId)
+        {
+            return dServicio.Eliminar(servicioId);
+        }
+
+        public List<Servicio> ListarTodo()
+        {
+            return dServicio.ListarTodo();
+        }
+
+    }
+}
---
+++ Negocio/NVehiculo.cs
@@ -0,0 +1,15 @@
+﻿using Datos;
+using System.Collections.Generic;
+
+namespace Negocio
+{
+    public class NVehiculo
+    {
+        private DVehiculo dVehiculo = new DVehiculo();
+
+        public List<Vehiculo> ListarTodo()
+        {
+            return dVehiculo.ListarTodo();
+        }
+    }
+}
